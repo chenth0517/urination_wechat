dc4c4627860338eeb5809c46594656b9d028d9df
chenth0517 <59386654+chenth0517@users.noreply.github.com>
2021-03-02 16:59:27 +0800

    微信小程序支持登录

--- miniprogram/pages/login/login.js
+++ miniprogram/pages/login/login.js
@@ -5,6 +5,24 @@ Page({
   },
   formSubmit: function (e) {
     console.log('form发生了submit事件，携带数据为：', e.detail.value)
+    wx.request({
+      url:'https://chenth.net/login_wx',
+      data:{
+        username:e.detail.value.username,
+        password:e.detail.value.password
+      },
+      method:'POST',
+      header:{
+        'content-type':'application/json'
+      },
+      success:function(res){
+        console.log("--------chenth success--------");
+        console.log(res.data);
+      },
+      fail:function(res){
+        console.log("--------chenth fail--------");
+      }
+    })
   },
   formReset: function () {
     console.log('form发生了reset事件')
