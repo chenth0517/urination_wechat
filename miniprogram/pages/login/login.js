b4efcf1bf12e1ee8f79bb3d42649386c02ce5c9e
chenth0517 <59386654+chenth0517@users.noreply.github.com>
2021-03-03 10:33:39 +0800

    微信小程序：登录并返回结果

--- miniprogram/pages/login/login.js
+++ miniprogram/pages/login/login.js
@@ -1,10 +1,16 @@
 //index.js
 Page({
+  data: {
+    result: 'asd'
+  },
+
   onLoad: function() {
     console.log("this is login page")
+    console.log(this.data.result)
   },
   formSubmit: function (e) {
     console.log('form发生了submit事件，携带数据为：', e.detail.value)
+    self=this
     wx.request({
       url:'https://chenth.net/login_wx',
       data:{
@@ -15,12 +21,19 @@ Page({
       header:{
         'content-type':'application/json'
       },
+      //self=this,
       success:function(res){
         console.log("--------chenth success--------");
         console.log(res.data);
+        self.setData({
+          result: res.data
+        })
       },
       fail:function(res){
         console.log("--------chenth fail--------");
+        self.setData({
+          result: res.data
+        })
       }
     })
   },
